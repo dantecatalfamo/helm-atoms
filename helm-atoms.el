cfb21f2240c6d386664c9bf3546caf89250ff8dc
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-09-15 19:30:28 -0400

    Init commit

---
+++ helm-atoms.el
@@ -0,0 +1,37 @@
+;;; helm-atoms.el -- Find atom using helm
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'helm)
+
+(defun helm-atoms--string-atom (atom)
+  "Return (name . value) for ATOMs that are strings."
+  (when (and (boundp atom)
+             (stringp (symbol-value atom))
+             (not (string-match-p "\n" (symbol-value atom))))
+    (cons (format "%s: %s" (propertize (symbol-name atom) 'face font-lock-variable-name-face) (symbol-value atom))
+          atom)))
+
+(defun helm-atoms--create-atom-list ()
+  "Create helm caididates for helm-atoms."
+  (let (candidates)
+    (mapatoms (lambda (atom)
+                (when-let (val (helm-atoms--string-atom atom))
+                  (push val candidates))))
+    candidates))
+
+(setq helm-source-string-atoms
+      (helm-build-sync-source "String atoms"
+        :candidates #'helm-atoms--create-atom-list
+        :action #'identity))
+
+(defun helm-atoms ()
+  "Use helm to search through all single line strinng atoms."
+  (interactive)
+  (helm :sources '(helm-source-string-atoms)))
+
+
+(provide 'helm-atoms)
+;;; helm-atoms.el ends here
