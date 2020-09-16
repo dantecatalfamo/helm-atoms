0fc2a1423ae4ac00b6ba41753cb97422ec107abd
Dante Catalfamo <43040593+dantecatalfamo@users.noreply.github.com>
2020-09-16 00:07:22 -0400

    Update helm-atoms.el

--- helm-atoms.el
+++ helm-atoms.el
@@ -11,7 +11,7 @@
 
 ;;; Commentary:
 
-;; Perform reverse variable lookups for Emacs Lisp using Helm.
+;; Perform an interactive reverse variable lookups for Emacs Lisp using Helm.
 ;; Works on numbers and strings, including multiline strings.
 ;; Once the helm entry is selected, the variable is opened in
 ;; either `describe-variable', or `helpful-variable', if it's installed.
