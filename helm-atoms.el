7e6f91a16f556c96ae1b0d1f965ea56861bb6372
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-10-13 13:23:28 -0400

    Remove old restriction on data types searched

--- helm-atoms.el
+++ helm-atoms.el
@@ -26,7 +26,6 @@
 ;;; Commentary:
 
 ;; Perform an interactive reverse variable lookups for Emacs Lisp using Helm.
-;; Works on numbers and strings, including multiline strings.
 ;; Once the helm entry is selected, the variable is opened in
 ;; either `describe-variable', or `helpful-variable', if it's installed.
 
